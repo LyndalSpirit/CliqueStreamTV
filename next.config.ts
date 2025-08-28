0b457586ec445bb92eef178ed032f63cb1d4d0e3
Lyndal Spirit <lyndalspirit@gmail.com>
2025-08-28 09:38:43 -0600

    Update next.config.ts
    New simple code
    
    Signed-off-by: Lyndal Spirit <lyndalspirit@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,16 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
+
